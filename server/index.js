3dde9409d4f3294e132c1adad03de5c2452dffc9
Kevin <kevin@cerda.io>
2018-05-24 00:47:42 -0700

    Began post implementation.

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
+const axios = require('axios');
+const parser = require('body-parser');
+const helper = require('../helpers/github.js')
+const save = require('../database/index.js')
 let app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
 
-app.post('/repos', function(req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+app.post('/repos', (req, res) => {
+  helper.getReposByUsername(req.body.data, (data) => {
+    //save data using save.save method
+  });
+  console.log('Requested data: ', req.body.data);
+  req.end('Success');
 });
 
-app.get('/repos', function(req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+app.get('/repos', (req, res) => {
+  console.log('Request:', req.body);
+  console.log('Response:', res.body);
+  res.send();
 });
 
 let port = 1128;
 
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
-});
+app.listen(port, () => console.log(`listening on port ${port}`));
