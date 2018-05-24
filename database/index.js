adc31d07ff09f21e587646539e6fb803609ede19
Kevin <kevin@cerda.io>
2018-05-24 00:29:15 -0700

    Implemented methods to log db connection status.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://kevin:test@ds133360.mlab.com:33360/fetcher');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Server connected'));
+
 let repoSchema = mongoose.Schema({
   id: Number,
   name: String,
